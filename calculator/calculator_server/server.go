2c18978e0cb762dd7801c37d2bb180327140aabd
viswamvs <viswavickymvs@gmail.com>
2022-03-31 09:56:06 +0530

    server modified

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -5,17 +5,31 @@ import (
 	calculator "grpc_course/calculator/calculatorpb"
 	"log"
 	"net"
-
+	"context"
 	"google.golang.org/grpc"
 )
 
 type server struct{
 	calculator.UnimplementedCalculatorServiceServer
 }
-func main() {
-	fmt.Print("hello")
 
-	lis, err := net.Listen("tcp", "0.0.0.0:8000")
+func (*server) Sum(ctx context.Context, req *calculator.SumRequest) (*calculator.SumResponse, error){
+	fmt.Printf("Received Sum RPC: %v", req)
+	a := req.Num1
+	b := req.Num2
+	sum := a + b
+
+	res := &calculator.SumResponse{
+		Result: sum,
+	}
+
+	return res, nil
+}
+
+func main() {
+	fmt.Print("Calculator Running")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 
 	if err != nil {
 		log.Fatalf("error: %v", err)
